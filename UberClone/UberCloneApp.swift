b6b768cd14b416d9b9fd91d6023d973e88145625
guavacadodev <jakewoodall001@gmail.com>
2026-01-10 23:45:38 -0800

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Jake Woodall on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UberCloneTests/UberCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneTests.swift
+//  UberCloneTests
+//
+//  Created by Jake Woodall on 1/10/26.
+//
+
+import Testing
+@testable import UberClone
+
+struct UberCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
